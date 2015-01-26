35d7519c24255d7bbeab745341ac91f8607e1bbf
Justin Mayfield <tooker@gmail.com>
2015-01-26 06:21:40 -0700

    Produce CSV wanrate output and make stable.
    wanrate now shows one line header with name of router
    and each subsequent line is just data.
    
    Fixed the ordering issues by keeping the row data in an ordered dict and
    rendering from that dict on each print..

--- ecmcli/commands/wanrate.py
+++ ecmcli/commands/wanrate.py
@@ -17,18 +17,21 @@ def command(api, args, routers):
     rfilter = {
         "id__in": ','.join(routers)
     }
+    column_fmt = '%20s'
+    header = [column_fmt % ('%s (%s)' % (x['name'], x['id']))
+              for x in routers.values()]
+    print(', '.join(header))
     while True:
         start = time.time()
         # XXX: We should calculate our own bps instead of using 'bps' to
         # ensure the resolution of our rate correlates with our sampletime.
         data = api.get('remote/status/wan/stats/bps', **rfilter)
         time.sleep(max(0, args.sampletime - (time.time() - start)))
-        row = []
         for x in data:
-            name = '%s(%s)' % (routers[str(x['id'])]['name'], x['id'])
             if x['success']:
                 value = humanize.naturalsize(x['data'], binary=True)
             else:
                 value = '[%s]' % x['reason']
-            row.append("%38s" % ('%s: %s' % (name, value)))
+            routers[str(x['id'])]['bps'] = value
+        row = [column_fmt % x['bps'] for x in routers.values()]
         print(', '.join(row))
