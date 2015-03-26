ece519d4e6d58b931f6e0e722e6b0509be3f9e46
Justin Mayfield <tooker@gmail.com>
2015-03-26 14:58:59 -0600

    First draft of interactive shell command.
    Usage:
        ecm shell -i <ROUTER_ID>
    
    This mode takes control of your tty to interact directly with a
    single remote Cradlepoint device.  Currently to exit you must
    kill the process externally.  I'll probably add a break sequence
    such as that used by ssh clients, a la., "~~".

--- ecmcli/commands/shell.py
+++ ecmcli/commands/shell.py
@@ -18,7 +18,8 @@ parser.add_argument('command', nargs="*", help='Command to execute')
 parser.add_argument('--interact', '-i', metavar='ROUTER_ID',
                     help='Interact directly with a single device')
 
-stdin = sys.stdin.fileno()
+raw_in = sys.stdin.buffer.raw
+raw_out = sys.stdout.buffer.raw
 
 
 def command(api, args, routers):
@@ -28,46 +29,69 @@ def command(api, args, routers):
         return bulk_session(api, args, routers)
 
 
+def window_size():
+    """ Returns width and height of window in characters. """
+    winsz = fcntl.ioctl(raw_in.fileno(), termios.TIOCGWINSZ, '1234')
+    return struct.unpack('hh', winsz)[::-1]
+
+
 def interactive_session(api, router):
-    h, w = struct.unpack('hh', fcntl.ioctl(stdin, termios.TIOCGWINSZ, '1234'))
-    print("Connecting to: Router %s" % router)
-    out = api.put('remote/control/csterm/ecmcli-%s' % api.sessionid, {
-        "w": w,
-        "h": h
-    }, id=router)[0]
+    stdin = sys.stdin.fileno()
     ttysave = termios.tcgetattr(stdin)
     tty.setraw(stdin)
+
+    attrs = termios.tcgetattr(stdin)
+    attrs[tty.IFLAG] = (attrs[tty.IFLAG] | termios.ICRNL)
+    attrs[tty.OFLAG] = (attrs[tty.OFLAG] | termios.ONLCR | termios.OPOST)
+    attrs[tty.LFLAG] = (attrs[tty.LFLAG] | termios.IEXTEN)
+    termios.tcsetattr(stdin, termios.TCSANOW, attrs)
+
     fl = fcntl.fcntl(stdin, fcntl.F_GETFL)
     fcntl.fcntl(stdin, fcntl.F_SETFL, fl | os.O_NONBLOCK)
     try:
         _interactive_session(api, router)
     finally:
-        termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, ttysave)
+        termios.tcsetattr(stdin, termios.TCSADRAIN, ttysave)
 
 
-def buffered_read(timeout=0.200):
-    """ Read from stdin up to an anthrophmoric timeout. """
+def buffered_read(timeout=0.500):
+    """ Read from stdin up to an anthropomorphic timeout. """
     buf = []
+    ts = time.time()
     while True:
-        ts = time.time()
-        if select.select([stdin], [], [], timeout-(time.time()-ts))[0]:
-            buf.append(sys.stdin.read())
+        to = timeout-(time.time()-ts)
+        if select.select([raw_in.fileno()], [], [], to)[0]:
+            buf.append(raw_in.read())
         else:
-            print("RETURN", buf)
-            return ''.join(buf)
+            return b''.join(buf)
 
 
 def _interactive_session(api, router):
+    print("Connecting to: Router %s" % router)
+    (w_save, h_save) = (w, h) = window_size()
+    api.put('remote/control/csterm/ecmcli-%s' % api.sessionid, {
+        "w": w,
+        "h": h
+    }, id=router)
     while True:
         in_data = buffered_read()
         while True:
+            w, h = window_size()
+            if (w, h) != (w_save, h_save):
+                api.put('remote/control/csterm/ecmcli-%s/' % api.sessionid, {
+                    "w": w,
+                    "h": h,
+                }, id=router)
+                w_save, h_save = w, h
             out = api.put('remote/control/csterm/ecmcli-%s/k' % api.sessionid,
-                          in_data, id=router)[0]
-            in_data = ""
+                          in_data.decode(), id=router)[0]
             if out['success']:
-                os.write(sys.stdout.fileno(), html.unescape(out['data']).encode())
+                if not out['data']:
+                    break
+                raw_out.write(html.unescape(out['data']).encode())
             else:
                 raise Exception('%s (%s)' % (out['exception'], out['reason']))
+            in_data = b""
 
 
 def bulk_session(api, args, routers):
