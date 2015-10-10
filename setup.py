347584c8f9b26853b0c0c1d13be7eda53042b184
Justin Mayfield <tooker@gmail.com>
2015-10-10 01:19:37 -0600

    Include python3.5 and just python in setuptools desc.

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ]
 )
